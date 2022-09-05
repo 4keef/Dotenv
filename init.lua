7602e455a762464e0591918ca33e0a5ec52b3d33
4keef <keef.devv@gmail.com>
2022-09-05 05:59:06 -0400

    Bug fix(2)

--- init.lua
+++ init.lua
@@ -4,33 +4,24 @@ local env = require('env')
 local function split(str, sep)
   sep = sep or "%s"
   local t = {}
-
   for v in string.gmatch(str, "([^"..sep.."]+)")
   do
     table.insert(t, v)
   end
-
   return t
 end
 
 local function load_env(path)
-  assert(
-    fs.existsSync(path),
-    "Invalid file path \""..path.."\""
-  )
-
+  assert(fs.existsSync(path), "Invalid file path \""..path.."\"")
   local file = io.open(path, "r")
-
   for line in file:lines()
   do
     local v = split(line, "=")
     env.set(unpack(v))
   end
-
   file:close()
   return true
 end
-
 
 return {
   load_env = load_env
