b698bc9ecd696844dd490686863545bbb5cf2d7b
4keef <keef.devv@gmail.com>
2023-01-25 18:59:34 -0400

    Two new functions load_env, set_value, get_value

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 local env = require 'env'
 local path = require 'path'
-local pretty = require 'pretty-print'
 local filesys = require 'fs'
-local env = require 'env'
+local process = require 'process'
+local pretty = require 'pretty-print'
 
 local M = {}
 
@@ -10,44 +10,53 @@ local stats = filesys.statSync
 local readdir = filesys.readdirSync
 local exists = filesys.existsSync
 
+local pattern = '([^"="]+)'
 
-function M.set_key(name, value)
+process = process.globalProcess()
 
+function M.set_value(key, value)
+  process.env[key] = value
+  return true
 end
 
---- Get value from environment
--- @param key (string) The name of the environment variable
--- @return (any) The value stored in the environment variable
 function M.get_value(key)
+  return process.env[key] or nil
 end
 
 function M.load_env(route)
-  if route and not exists(route) then
-    error("Not valid file path \"" .. route .. "\" OR it does not exists", 1)
+  if route then
+    if not exists(route) then
+      error("Unable to find path \"" .. route .. "\"")
+    end
   else
     -- Path of current directory
     route = path.resolve()
   end
 
-  io.write(route)
   -- Returns data on the file/dir
   local stat = stats(route)
 
-  pretty.prettyPrint(stat)
-
   if stat and stat.type == "directory" then
-    M.load_env(path.join(route, '.env'))
+    return M.load_env(path.join(route, '.env'))
   end
 
   local dotenv = io.open(route, "r")
-  local content = io.read(dotenv)
 
-  io.write(content)
+  -- Parse content of file
+  local parsed = {}
 
+  for line in dotenv:lines() do
+    local inner = {}
 
+    for value in string.gmatch(line, pattern) do
+      table.insert(inner, value)
+    end
+
+    table.insert(parsed, inner)
+  end
+
+  for _, p in ipairs(parsed) do M.set_key(unpack(p)) end
   return io.close(dotenv)
 end
 
-
-M.load_env("./.env")
 return M
