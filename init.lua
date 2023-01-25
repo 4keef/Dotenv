f69b5cb7cedb107d9a0f8b0fa6d3211cf3395abe
4keef <keef.devv@gmail.com>
2023-01-25 15:48:02 -0400

    update fix

--- init.lua
+++ init.lua
@@ -1,27 +1,53 @@
-local fs = require('fs')
-local env = require('env')
+local env = require 'env'
+local path = require 'path'
+local pretty = require 'pretty-print'
+local filesys = require 'fs'
+local env = require 'env'
 
-local function split(str, sep)
-  sep = sep or "%s"
-  local t = {}
-  for v in string.gmatch(str, "([^"..sep.."]+)")
-  do
-    table.insert(t, v)
-  end
-  return t
+local M = {}
+
+local stats = filesys.statSync
+local readdir = filesys.readdirSync
+local exists = filesys.existsSync
+
+
+function M.set_key(name, value)
+
 end
 
-local function load_env(path)
-  assert(fs.existsSync(path), "Invalid file path \""..path.."\"")
-  local file = io.open(path, "r")
-  for line in file:lines()
-  do
-    env.set(unpack(split(line, "=")))
-  end
-  file:close()
-  return true
+--- Get value from environment
+-- @param key (string) The name of the environment variable
+-- @return (any) The value stored in the environment variable
+function M.get_value(key)
 end
 
-return {
-  load_env = load_env
-}
+function M.load_env(route)
+  if route and not exists(route) then
+    error("Not valid file path \"" .. route .. "\" OR it does not exists", 1)
+  else
+    -- Path of current directory
+    route = path.resolve()
+  end
+
+  io.write(route)
+  -- Returns data on the file/dir
+  local stat = stats(route)
+
+  pretty.prettyPrint(stat)
+
+  if stat and stat.type == "directory" then
+    M.load_env(path.join(route, '.env'))
+  end
+
+  local dotenv = io.open(route, "r")
+  local content = io.read(dotenv)
+
+  io.write(content)
+
+
+  return io.close(dotenv)
+end
+
+
+M.load_env("./.env")
+return M
